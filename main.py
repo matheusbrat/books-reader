76866ce92444b7ec743f7d71df63ed96a6a1bc5a
Matheus Bratfisch <matheus@ckl.io>
2016-07-09 21:29:22 -0300

    Removing unecessary prints adding .env-example file

--- main.py
+++ main.py
@@ -8,15 +8,12 @@ def main():
     bl = BooksLibrary(BOOKS_PATH)
     bl.remote_sync()
     page = bl.generate_page()
-    print(page, type(page))
     with open('page.md', 'w+') as f:
         f.write(page)
 
     command = ["sh", POST_PAGE_GENERATED, abspath('page.md')]
-    print(command)
     process = subprocess.Popen(command, stdout=subprocess.PIPE)
     output = process.communicate()[0]
-    print(output)
 
 
 if __name__ == "__main__":
